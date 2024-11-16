197b6d2ca9c9764536750333f0b3b19e79eea028
TheFutaBandit <prameshanand2021@gmail.com>
2024-11-16 20:40:18 +0530

    Added WeatherApp Configuration basic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import "./styles.css"
 import {weather_extraction} from "./weatherConfig.js";
 
-const temp = document.querySelector("h1");
+// const temp = document.querySelector("h1");
 
-let data = weather_extraction();
-let b = data.weather_current;
-console.log(b);
+let data = await weather_extraction("New Delhi");
+console.log(data.temperature_current());
+console.log(data);
+// data.data_current();
+// console.log(data.weather_current().conditions);
+
+// console.log(data);
+// let hi = data.sayHello();
+// console.log(hi);
+// let b = await data.weather_current();
+// console.log(b);
+
 
 
--- src/weatherConfig.js
+++ src/weatherConfig.js
@@ -1,28 +1,26 @@
 export const weather_extraction = async (location) => {
-    let weather_data_object = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New Delhi?key=DCZD5EQRWC7NRZQN93UXQFWFA")
+    let weather_data_object = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DCZD5EQRWC7NRZQN93UXQFWFA`)
     let weather_data = await weather_data_object.json();
+    let current_condition = weather_data.currentConditions;
+    // console.log(current_condition);
 
-    const weather_current = async () => {
-        return await weather_data.currentConditions;
+    const data_current = () => {
+        console.log(weather_data);
     }
 
-    console.log(weather_data);
-    // console.log(weather_data.currentConditions.conditions);
-    console.log()
+    const weather_current = () => {
+        return current_condition;
+    }
+
+    const temperature_current = () => {
+        return current_condition.temp;
+    }
 
     return {
         weather_current,
+        temperature_current,
+        data_current,
     }
+
 }
-
-// class weather_extraction {
-//     constructor(location) {
-//         this.location = location;
-//     }
-
-//     async weather_extraction() {
-//         let weather_data_object = await fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New Delhi?key=DCZD5EQRWC7NRZQN93UXQFWFA")
-//         let weather_data = await weather_data_object.json();
-//     }
-// }
 
