6a1237f3c0665b9b6191ab397281ca9ffbe78ad9
TheFutaBandit <prameshanand2021@gmail.com>
2024-11-17 19:26:03 +0530

    description tile finished

--- src/index.js
+++ src/index.js
@@ -13,6 +13,19 @@ console.log(data);
 
 const temperature = document.querySelector(".temperature");
 temperature.textContent = toggleTemp(data.temperature_current()).toFixed(1);
+
+const location = document.querySelector(".location");
+location.textContent = data.address_current();
+
+const condition = document.querySelector(".status");
+condition.textContent = data.condition_current();
+
+const time = document.querySelector(".time");
+time.textContent = data.time_current();
+
+console.log(data.icon_current());
+
+
 // data.data_current();
 // console.log(data.weather_current().conditions);
 
--- src/weatherConfig.js
+++ src/weatherConfig.js
@@ -3,6 +3,7 @@ export const weather_extraction = async (location) => {
     let weather_data = await weather_data_object.json();
     let current_condition = weather_data.currentConditions;
     // console.log(current_condition);
+    console.log(weather_data);
 
     const data_current = () => {
         console.log(weather_data);
@@ -16,10 +17,30 @@ export const weather_extraction = async (location) => {
         return current_condition.temp;
     }
 
+    const address_current = () => {
+        return weather_data.address;
+    }
+
+    const condition_current = () => {
+        return current_condition.conditions;
+    }
+
+    const time_current = () => {
+        return current_condition.datetime;
+    }
+
+    const icon_current = () => {
+        return current_condition.icon;
+    }
+
     return {
         weather_current,
         temperature_current,
         data_current,
+        address_current,
+        condition_current,
+        time_current,
+        icon_current
     }
 
 }
