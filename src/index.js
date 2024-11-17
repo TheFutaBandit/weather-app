fc89ccfa4f88c9058fb3caede6fcec18f42bcfa0
TheFutaBandit <prameshanand2021@gmail.com>
2024-11-17 18:22:56 +0530

    temperature tile now updates current temperature

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import {weather_extraction} from "./weatherConfig.js";
 
 // const temp = document.querySelector("h1");
 
+let toggleTemp = (temp) => {
+    return (((temp - 32) * 5)/9);
+}
+
 let data = await weather_extraction("New Delhi");
 console.log(data.temperature_current());
 console.log(data);
+
+const temperature = document.querySelector(".temperature");
+temperature.textContent = toggleTemp(data.temperature_current()).toFixed(1);
 // data.data_current();
 // console.log(data.weather_current().conditions);
 
